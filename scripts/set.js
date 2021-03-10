ce9c95522334df9fa7cba03dc06c281286152d61
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2021-03-09 20:45:05 -0500

    swaps between views with "start" and "back to main" button

--- scripts/set.js
+++ scripts/set.js
@@ -1,8 +1,32 @@
-function pageLoad() {
-    alert("testing");
-    // code goes here
+window.onload = function() {
 
-    // function calls 
+    /* GLOBAL VARIABLES */
+    const menuView = document.getElementById("menu-view");  // main menu screen
+    const gameView = document.getElementById("game-view");  // game screen
+    const startGame = document.getElementById("start");     // start button
+    const mainMenu = document.getElementById("back");       //main menu button
+
+    /* FUNCTION DEFINITIONS */
+    function game() {   //swapping visibility from menu to game by adding and removing a hidden class
+        menuView.classList.add("hidden");
+        gameView.classList.remove("hidden");
+    }
+
+    function menu() {   // swapping visibility from game to menu by adding and removing a hidden class
+        gameView.classList.add("hidden");
+        menuView.classList.remove("hidden");
+    }
+
+    /* MAIN FUNCTION - Beginning of Program */
+    function main() {
+        startGame.addEventListener("click", function() {
+            game();
+        })
+
+        mainMenu.addEventListener("click", function() {
+            menu();
+        })
+    }
+    
+    main();
 }
-
-window.onload = pageLoad;
